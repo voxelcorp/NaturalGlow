746e94e72b039ecf893d50dafcc3baf4469b2c2f
voxelcorp <andre.fernandesdev@gmail.com>
2021-07-23 12:44:12 +0100

    Delete all

--- api/models/ingredients.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-var ingredientSchema = new mongoose.Schema({
-  name: {type: String, required: true, unique: true},
-});
-
-mongoose.model('Ingredient', ingredientSchema);
